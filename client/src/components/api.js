50fdb9dce98d3aa6e6cc93747e3d0a8a3a8ff8b9
Soumen Nath <saumennath29@gmail.com>
2021-09-06 00:25:39 +0530

    fixing bug 5

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let api = axios.create({
-    baseURL:"http://localhost:5000/api/",
+    baseURL:"/api/",
     headers:{
         "x-access-token" :localStorage.getItem("token=f9jCrLBFEFSseA^3")
     }
