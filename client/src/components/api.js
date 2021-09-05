767f7f2f85a165c849a9f0933f3f574022ae807f
Soumen Nath <saumennath29@gmail.com>
2021-09-06 00:03:40 +0530

    fixing bug 4

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,13 +1,6 @@
 import axios from 'axios'
-
-if(process.env.NODE_ENV === "production") {
-    SERVER_URI = 'HEROKU_URI'
-} 
-else if(process.env.NODE_ENV === "development") {
-    SERVER_URI = 'localhost:5000'
-}
 let api = axios.create({
-    baseURL:`http://${SERVER_URI}/api/`,
+    baseURL:"http://localhost:5000/api/",
     headers:{
         "x-access-token" :localStorage.getItem("token=f9jCrLBFEFSseA^3")
     }
