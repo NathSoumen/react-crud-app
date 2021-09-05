d388ced75880f68cfec7bd1fb42f4c7bbc16c78c
Soumen Nath <saumennath29@gmail.com>
2021-09-06 00:20:17 +0530

    fixing bug 5

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.use(cors())
 app.use(expressSanitizer());
 app.use(passport.initialize())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
 app.use("/api/users", userRoutes)
 app.use("/api/memo",verifyJWT, memoRoutes)
 
